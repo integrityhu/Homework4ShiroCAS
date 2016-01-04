5352f800f2d4de3b66966fa1dbd8047949d96bf7
Papp Zoltán <papp.zoltan@integrity.hu>
2016-01-04 14:40:00 +0100

    init

---
+++ Homework4ShiroCAS/src/main/java/hu/infokristaly/homework/shirocas/AuthBack.java
@@ -0,0 +1,11 @@
+package hu.infokristaly.homework.shirocas;
+
+/*
+ * http://blog.pinateknoloji.com/shiro-jdbc-realm
+ * http://shiro.apache.org/cas.html
+ * https://github.com/casinthecloud/java-shiro-cas-client-demo
+ * 
+ */
+public class AuthBack {
+
+}
